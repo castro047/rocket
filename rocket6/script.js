d719df1604553e03e06cfc805c3a9af08cc41c2b
gustavo <105922570+castro047@users.noreply.github.com>
2023-05-30 17:59:09 -0300

    Add files via upload

---
+++ rocket6/script.js
@@ -0,0 +1,38 @@
+class Parafuso {
+  constructor() {
+    if (this.constructor === Parafuso)
+      throw new Error("Classe abstrata não pode ser instantanciada");
+  }
+  get tipo() {
+    throw new Error('Metodo "get tipo()" precisa ser implementado');
+  }
+}
+
+class Fenda extends Parafuso {
+  constructor() {
+    super();
+  }
+
+  get tipo() {
+    return "fenda";
+  }
+}
+class Philips extends Parafuso {
+  constructor() {
+    super();
+  }
+
+  get tipo() {
+    return "philips";
+  }
+}
+
+class Allen extends Parafuso {}
+
+new Parafuso();
+let fenda = new Fenda();
+let philips = new Philips();
+let allen = new Allen();
+
+console.log(fenda.tipo, philips.tipo);
+console.log(allen.tipo);
