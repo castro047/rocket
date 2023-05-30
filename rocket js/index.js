405941678313e23fe77c7a5b373668aad7bfe6d2
gustavo <105922570+castro047@users.noreply.github.com>
2023-05-30 17:58:34 -0300

    Add files via upload

---
+++ rocket js/index.js
@@ -0,0 +1,18 @@
+let aceitar = true;
+
+console.log("pedir uber");
+
+const promessa = new Promise((resolve, reject) => {
+  if (aceitar) {
+    return resolve("pedido aceito!");
+  }
+
+  return reject("pedido negado!");
+});
+
+console.log("aguardando");
+
+promessa
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("finalizada"));
